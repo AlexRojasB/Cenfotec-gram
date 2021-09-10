5ec7b3a75a9b8718424b9f89033b173e1b87bea3
Alexander Rojas <alexrrojas.b@gmail.com>
2021-09-09 19:26:42 -0600

    init final project

---
+++ UCenfotec-gram/UCenfotec-gram/UCenfotec_gramApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCenfotec_gramApp.swift
+//  UCenfotec-gram
+//
+//  Created by Alexander Rojas Benavides on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct UCenfotec_gramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
