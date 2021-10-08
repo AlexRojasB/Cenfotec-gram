4a86c397da91e4947fa5fb90314c164a90e7c9c2
Alexander Rojas <alexrrojas.b@gmail.com>
2021-10-07 23:17:35 -0600

    Add new swiftui files for next pages

---
+++ UCenfotec-gram/UCenfotec-gram/App/SigninView.swift
@@ -0,0 +1,20 @@
+//
+//  SigninView.swift
+//  UCenfotec-gram
+//
+//  Created by Alexander Rojas Benavides on 10/7/21.
+//
+
+import SwiftUI
+
+struct SigninView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SigninView_Previews: PreviewProvider {
+    static var previews: some View {
+        SigninView()
+    }
+}
